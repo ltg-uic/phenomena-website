dc528c4c071b5b718c5563074b4633af46950a1e
Fred Kilbourn <fred@fredk.com>
2012-04-08 00:44:55 -0500

    Transitioned from Ruby to PHP, initial commit of new codebase.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-PhenomenaNew::Application.config.secret_token = 'a5127a5c06989174ab5ef48e4f528e0af3197c94dbe705912b3c57c0849dad5ade19a56d4e8f5c7c6524db929e60e2a6b16997f831e89c114361c8d0ac3cae1f'
