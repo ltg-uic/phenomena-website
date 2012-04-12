700a83a2bc5431c5a6a3dd20947c25ec1bba2bf2
Fred Kilbourn <fred@fredk.com>
2012-04-12 01:52:08 -0500

    Added dist config

---
+++ config.dist.php
@@ -0,0 +1,12 @@
+<?php
+$GLOBALS['timezone'] = "";
+$GLOBALS['dbHost'] = "";
+$GLOBALS['dbName'] = "";
+$GLOBALS['dbUser'] = "";
+$GLOBALS['dbPass'] = "";
+$GLOBALS['xmppDomain'] = "";
+$GLOBALS['xmppUser'] = "";
+$GLOBALS['xmppPass'] = "";
+$GLOBALS['xmppLogPath'] = "";
+$GLOBALS['xmppPidPath'] = "";
+?>
